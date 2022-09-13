e81ce85bc1d1d928e43458f6c8d0cbc16c6a63d0
tabearoeber <t.e.rober@uva.nl>
2022-09-13 09:11:53 +0200

    Update Datasets.py

--- Datasets.py
+++ Datasets.py
@@ -89,6 +89,262 @@ def adult(wd):
     return df, df_train, df_test, d, clf, df_factuals, model_master, encoder, scaler
 
 
+
+def heloc(wd):
+    ## load data
+    df = pd.read_csv(wd+'heloc/dataset_carla_heloc.csv')
+    df_train = pd.read_csv(wd + 'heloc/train_carla_heloc.csv')
+    df_test = pd.read_csv(wd + 'heloc/test_carla_heloc.csv')
+
+    ## load readme file
+    try:
+        fhand = open(wd + 'heloc/README.txt')
+    except:
+        print("File cannot be opened.")
+        exit()
+
+    d = dict()
+
+    for line in fhand:
+        # line = line.strip()
+        # words = line.split()
+        line = line.strip()
+        line = line.replace('[', '')
+        line = line.replace(']', '')
+        line = line.replace(',', '')
+        line = line.replace("'", '')
+        words = line.split()
+        if line.startswith("categorical:"):
+            d['categorical'] = words[1:]
+
+        elif line.startswith('continuous:'):
+            d['numerical'] = words[1:]
+
+        elif line.startswith('immutables:'):
+            d['immutable'] = words[1:]
+
+        elif line.startswith('target:'):
+            d['target'] = words[1]
+
+    ## load predictive model
+    path = wd + 'heloc/RF_heloc.pkl'
+    with open(path, 'rb') as f:
+        clf = pickle.load(f)
+
+    ## load factual instances
+    df_factuals = pd.read_csv(wd + 'heloc/factuals_heloc.csv')
+
+    ## load encoder
+    path = wd + 'heloc/encoder_heloc'
+    with open(path, 'rb') as f:
+        encoder = pickle.load(f)
+
+    ## load scaler
+    path = wd + 'heloc/scaler_heloc'
+    with open(path, 'rb') as f:
+        scaler = pickle.load(f)
+
+
+    ## Save model
+    y_train = df_train[d['target']]
+    X_train = df_train.drop(d['target'], axis=1)
+
+    if not os.path.exists('results_carla/'):
+        os.makedirs('results_carla/')
+
+    constraintL = oc.ConstraintLearning(X_train, y_train, clf, 'rf')
+    constraint_add = constraintL.constraint_extrapolation('binary')
+    constraint_add.to_csv('results_carla/%s_%s_model.csv' % ('rf', 'heloc'), index=False)
+
+    ## create model master
+    m = {
+        'outcome': 'counterfactual_heloc',
+        'model_type': 'rf',
+        'save_path': 'results_carla/%s_%s_model.csv' % ('rf', 'heloc'),
+        'task': 'binary',
+        'objective': 0,
+        'lb': 0.5,
+        'ub': None,
+        'SCM_counterfactuals': None,
+        'features': [[col for col in df.columns.drop(d['target'])]]
+    }
+
+    model_master = pd.DataFrame(m)
+
+    return df, df_train, df_test, d, clf, df_factuals, model_master, encoder, scaler
+
+
+
+def compas(wd):
+    ## load data
+    df = pd.read_csv(wd+'compas/dataset_carla_compas.csv')
+    df_train = pd.read_csv(wd + 'compas/train_carla_compas.csv')
+    df_test = pd.read_csv(wd + 'compas/test_carla_compas.csv')
+
+    ## load readme file
+    try:
+        fhand = open(wd + 'compas/README.txt')
+    except:
+        print("File cannot be opened.")
+        exit()
+
+    d = dict()
+
+    for line in fhand:
+        # line = line.strip()
+        # words = line.split()
+        line = line.strip()
+        line = line.replace('[', '')
+        line = line.replace(']', '')
+        line = line.replace(',', '')
+        line = line.replace("'", '')
+        words = line.split()
+        if line.startswith("categorical:"):
+            d['categorical'] = words[1:]
+
+        elif line.startswith('continuous:'):
+            d['numerical'] = words[1:]
+
+        elif line.startswith('immutables:'):
+            d['immutable'] = words[1:]
+
+        elif line.startswith('target:'):
+            d['target'] = words[1]
+
+    ## load predictive model
+    path = wd + 'compas/RF_compas.pkl'
+    with open(path, 'rb') as f:
+        clf = pickle.load(f)
+
+    ## load factual instances
+    df_factuals = pd.read_csv(wd + 'compas/factuals_compas.csv')
+
+    ## load encoder
+    path = wd + 'compas/encoder_compas'
+    with open(path, 'rb') as f:
+        encoder = pickle.load(f)
+
+    ## load scaler
+    path = wd + 'compas/scaler_compas'
+    with open(path, 'rb') as f:
+        scaler = pickle.load(f)
+
+
+    ## Save model
+    y_train = df_train[d['target']]
+    X_train = df_train.drop(d['target'], axis=1)
+
+    if not os.path.exists('results_carla/'):
+        os.makedirs('results_carla/')
+
+    constraintL = oc.ConstraintLearning(X_train, y_train, clf, 'rf')
+    constraint_add = constraintL.constraint_extrapolation('binary')
+    constraint_add.to_csv('results_carla/%s_%s_model.csv' % ('rf', 'compas'), index=False)
+
+    ## create model master
+    m = {
+        'outcome': 'counterfactual_compas',
+        'model_type': 'rf',
+        'save_path': 'results_carla/%s_%s_model.csv' % ('rf', 'compas'),
+        'task': 'binary',
+        'objective': 0,
+        'lb': 0.5,
+        'ub': None,
+        'SCM_counterfactuals': None,
+        'features': [[col for col in df.columns.drop(d['target'])]]
+    }
+
+    model_master = pd.DataFrame(m)
+
+    return df, df_train, df_test, d, clf, df_factuals, model_master, encoder, scaler
+
+
+
+def give_me_some_credit(wd):
+    ## load data
+    df = pd.read_csv(wd+'give_me_some_credit/dataset_carla_give_me_some_credit.csv')
+    df_train = pd.read_csv(wd + 'give_me_some_credit/train_carla_give_me_some_credit.csv')
+    df_test = pd.read_csv(wd + 'give_me_some_credit/test_carla_give_me_some_credit.csv')
+
+    ## load readme file
+    try:
+        fhand = open(wd + 'give_me_some_credit/README.txt')
+    except:
+        print("File cannot be opened.")
+        exit()
+
+    d = dict()
+
+    for line in fhand:
+        # line = line.strip()
+        # words = line.split()
+        line = line.strip()
+        line = line.replace('[', '')
+        line = line.replace(']', '')
+        line = line.replace(',', '')
+        line = line.replace("'", '')
+        words = line.split()
+        if line.startswith("categorical:"):
+            d['categorical'] = words[1:]
+
+        elif line.startswith('continuous:'):
+            d['numerical'] = words[1:]
+
+        elif line.startswith('immutables:'):
+            d['immutable'] = words[1:]
+
+        elif line.startswith('target:'):
+            d['target'] = words[1]
+
+    ## load predictive model
+    path = wd + 'give_me_some_credit/RF_give_me_some_credit.pkl'
+    with open(path, 'rb') as f:
+        clf = pickle.load(f)
+
+    ## load factual instances
+    df_factuals = pd.read_csv(wd + 'give_me_some_credit/factuals_give_me_some_credit.csv')
+
+    ## load encoder
+    path = wd + 'give_me_some_credit/encoder_give_me_some_credit'
+    with open(path, 'rb') as f:
+        encoder = pickle.load(f)
+
+    ## load scaler
+    path = wd + 'give_me_some_credit/scaler_give_me_some_credit'
+    with open(path, 'rb') as f:
+        scaler = pickle.load(f)
+
+
+    ## Save model
+    y_train = df_train[d['target']]
+    X_train = df_train.drop(d['target'], axis=1)
+
+    if not os.path.exists('results_carla/'):
+        os.makedirs('results_carla/')
+
+    constraintL = oc.ConstraintLearning(X_train, y_train, clf, 'rf')
+    constraint_add = constraintL.constraint_extrapolation('binary')
+    constraint_add.to_csv('results_carla/%s_%s_model.csv' % ('rf', 'give_me_some_credit'), index=False)
+
+    ## create model master
+    m = {
+        'outcome': 'counterfactual_give_me_some_credit',
+        'model_type': 'rf',
+        'save_path': 'results_carla/%s_%s_model.csv' % ('rf', 'give_me_some_credit'),
+        'task': 'binary',
+        'objective': 0,
+        'lb': 0.5,
+        'ub': None,
+        'SCM_counterfactuals': None,
+        'features': [[col for col in df.columns.drop(d['target'])]]
+    }
+
+    model_master = pd.DataFrame(m)
+
+    return df, df_train, df_test, d, clf, df_factuals, model_master, encoder, scaler
+
+
+
 def label(row, filter_col):
     if any(row[filter_col] == 1):
         return 0
